5f1a85bd10fe05fd99cf41b73267d13a146bc003
Anshita Sayal <asayal@ncsu.edu>
2015-11-28 03:35:36 -0500

    Update installRVMonitor.sh

--- vm-prerequisites/installRVMonitor.sh
+++ vm-prerequisites/installRVMonitor.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Install script for RV-Monitor
+
+#Downloading RV-Monitor from official website
 wget --directory-prefix="/home/vagrant/Downloads" http://runtimeverification.com/monitor/1.3/rv-monitor-installer-1.3.jar
 cd /home/vagrant/Downloads
+
+#Running the executable
 java -jar rv-monitor-installer-1.3.jar
